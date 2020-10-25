080f710693608de7b24945690c320f26ae9eb42f
mygithubnelsonr <mygithub@sys-service.de>
2020-10-25 18:31:13 +0100

    Add ValidationExamples

--- WpfValidationExample/WpfValidationExample/MainWindow.xaml.cs
+++ WpfValidationExample/WpfValidationExample/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 
 namespace WpfValidationExample
@@ -24,6 +11,13 @@ namespace WpfValidationExample
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonPropertyTrigger_Click(object sender, RoutedEventArgs e)
+        {
+            PropertyTrigger.Dialog dialog = new PropertyTrigger.Dialog();
+            dialog.Owner = this;
+            dialog.ShowDialog();
         }
 
         private void ButtonValidationWithException_Click(object sender, RoutedEventArgs e)
@@ -46,5 +40,7 @@ namespace WpfValidationExample
             dlg.Owner = this;
             dlg.ShowDialog();
         }
+
+
     }
 }
